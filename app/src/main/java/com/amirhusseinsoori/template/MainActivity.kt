329b788617a4d374e802f0dbbb08e4f4a62662ec
amirhussein soori <amirhusseinsoori75@gmail.com>
2021-01-23 20:38:02 +0330

    create keep stateNavigator

--- app/src/main/java/com/amirhusseinsoori/template/MainActivity.kt
+++ app/src/main/java/com/amirhusseinsoori/template/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.amirhusseinsoori.template
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -8,7 +9,9 @@ import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.plusAssign
 import androidx.navigation.ui.*
+import com.amirhusseinsoori.template.util.KeepStateNavigator
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,13 +19,27 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.findNavController()
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+//        navController = navHostFragment.findNavController()
+
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        // get fragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)!!
+
+        // setup custom navigator
+        val navigator = KeepStateNavigator(this, navHostFragment.childFragmentManager, R.id.nav_host_fragment)
+        navController.navigatorProvider += navigator
+
+        // set navigation graph
+        navController.setGraph(R.navigation.nav_graph)
 
 
 
@@ -30,13 +47,13 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.drawer_nav_bottom, menu)
-        return true
-    }
-    override fun onSupportNavigateUp(): Boolean = navController.navigateUp() || super.onSupportNavigateUp()
-    override fun onOptionsItemSelected(item: MenuItem): Boolean  = item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
-
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.drawer_nav_bottom, menu)
+//        return true
+//    }
+//    override fun onSupportNavigateUp(): Boolean = navController.navigateUp() || super.onSupportNavigateUp()
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean  = item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+//
 
 
 
